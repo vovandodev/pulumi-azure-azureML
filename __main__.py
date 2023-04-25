b4118b68918dc8f145c37c42b8e589a0eb5c385e
Volodymyr Vrublevskyy <vovandos@gmail.com>
2023-04-25 15:14:45 +0200

    first commit

---
+++ __main__.py
@@ -0,0 +1,33 @@
+# __main__.py
+
+import pulumi
+from pulumi_azure_native import resources
+from pulumi_azure_native import operationalinsights
+from pulumi_azure_native import machinelearningservices
+from pulumi_azure_native import storage
+from pulumi_azure_native import datalakestore
+
+import config
+
+# Get the resource group by name
+resource_group = resources.ResourceGroup.get(config.RESOURCE_GROUP_NAME, config.RESOURCE_GROUP_NAME)
+
+# Create an Azure Storage Account
+storage_account = storage.StorageAccount(config.STORAGE_ACCOUNT_NAME, 
+    resource_group_name=resource_group.name,
+    sku=storage.SkuArgs(
+        name=storage.SkuName.STANDARD_LRS,
+    ),
+    kind=storage.Kind.STORAGE_V2)
+
+# Create an Azure Machine Learning Workspace
+aml_workspace = machinelearningservices.Workspace(config.WORKSPACE_NAME,
+    resource_group_name=resource_group.name,
+    location=config.LOCATION,
+    friendly_name=config.WORKSPACE_NAME,
+    sku=machinelearningservices.SkuArgs(
+        name=machinelearningservices.SkuName.BASIC,
+        tier=config.SKU_TIER
+    ),
+    storage_account=storage_account.id
+)
---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+RESOURCE_GROUP_NAME = "rg-edocommunitypoctest-cus-op78"
+LOCATION = "centralus"
+STORAGE_ACCOUNT_NAME = "vv-amlstorage"
+WORKSPACE_NAME = "vvAMLWorkspace"
+SKU_TIER = "basic"
